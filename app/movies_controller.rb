e44f30f686509886e780cf51a2b0bd807195ef75
Arye Dov Eidelman <aryedov.e@gmail.com>
2018-05-09 18:56:26 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -25,7 +25,7 @@ end
 def can_be_created_in_a_block(args = {title: 'Home Alone', release_date: 1990})
   Movie.create do |m|
     m.title = args[:title]
-    m.release_date = release_date[:release_date]
+    m.release_date = args[:release_date]
     m.director = args[:director]
     m.lead = args[:lead]
     m.in_theaters = args[:in_theaters]
